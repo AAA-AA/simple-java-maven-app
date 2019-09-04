1367e03e181d5ce69653c411c094a28198d2cd3e
renhongqiang <renhongqiang@megvii.com>
2019-09-04 17:41:45 +0800

    for auto compile test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,9 +4,7 @@ package com.mycompany.app;
  * Hello world!
  */
 public class App {
-
     private final String message = "Hello World! RRR";
-
     public App() {}
 
     public static void main(String[] args) {
